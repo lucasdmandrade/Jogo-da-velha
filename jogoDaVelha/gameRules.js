3fb0cc5613254830ba0509f9ff1b7044efe74c0e
lucasdmandrade <lucasdmandrade@gmail.com>
2021-10-06 20:03:24 -0300

    Centralizado add teste para valores e travamento

--- jogoDaVelha/gameRules.js
+++ jogoDaVelha/gameRules.js
@@ -1,101 +1,92 @@
 //Função para puxar o valor digitado e o id do seu input
-function pullValue(id){
+function pullValue(id) {
     let button = document.getElementById(id)
-    if(round % 2 == 0){
-        button.value = player1.value
-        document.getElementById(`${id}`).innerHTML = `
-    <p> ${player1.value} </p>
-    `
-    }else{
-        button.value = player2.value
-        document.getElementById(`${id}`).innerHTML = `
-    <p> ${player2.value.substr(0, 1)} </p>
-    `
-    }
+    showValue(id, button)
+    setValues(id)
+
     value = document.getElementById(id).value
-    
 
     testVictory(id, value)
     round = contRounds(round)
-    if(victory != true){
+    if (victory != true) {
         whoPlays(round)
-    }else{
+    } else {
         crashInputs()
     }
-}  
+}
 
 //Função para testar a cada clique se o jogador venceu
-function testVictory(id, value){
+function testVictory(id, value) {
 
     id = Number(id)
-    console.log(typeof(id))
+    console.log(typeof (id))
 
-    if(id == 5){
-        if(document.getElementById(1).value == value  && document.getElementById(9).value == value){
-            victory = colorizeWinner(id, 1, 9, value);  
-        }else if(document.getElementById(3).value == value  && document.getElementById(7).value == value){
-            victory = colorizeWinner(id, 3, 7, value);  
-        }else if(document.getElementById(2).value == value && document.getElementById(8).value == value){
-            victory = colorizeWinner(id, 2, 8, value);  
-        }else if(document.getElementById(4).value == value && document.getElementById(6).value == value){
-            victory = colorizeWinner(id, 4, 6, value);  
+    if (id == 5) {
+        if (document.getElementById(1).value == value && document.getElementById(9).value == value) {
+            victory = colorizeWinner(id, 1, 9, value);
+        } else if (document.getElementById(3).value == value && document.getElementById(7).value == value) {
+            victory = colorizeWinner(id, 3, 7, value);
+        } else if (document.getElementById(2).value == value && document.getElementById(8).value == value) {
+            victory = colorizeWinner(id, 2, 8, value);
+        } else if (document.getElementById(4).value == value && document.getElementById(6).value == value) {
+            victory = colorizeWinner(id, 4, 6, value);
         }
-        
-    }else if(id == 1 || id == 4 || id == 7){
+
+    } else if (id == 1 || id == 4 || id == 7) {
         //Diagonal
-        if(id == 1){
-            if(document.getElementById(1).value == document.getElementById(5).value && document.getElementById(5).value  == document.getElementById(9).value){
-                victory = colorizeWinner(id, 5, 9, value);  
+        if (id == 1) {
+            if (document.getElementById(1).value == document.getElementById(5).value && document.getElementById(5).value == document.getElementById(9).value) {
+                victory = colorizeWinner(id, 5, 9, value);
             }
             //Diagonal
-        }else if(id == 7){
-            if(document.getElementById(7).value == document.getElementById(5).value && document.getElementById(5).value  == document.getElementById(3).value){
-                victory = colorizeWinner(id, 5, 3, value);  
+        } else if (id == 7) {
+            if (document.getElementById(7).value == document.getElementById(5).value && document.getElementById(5).value == document.getElementById(3).value) {
+                victory = colorizeWinner(id, 5, 3, value);
             }
         }
         //Lateral
-        if(document.getElementById(id + 1).value == value  && document.getElementById(id +2).value == value){
-            victory = colorizeWinner(id, id+1, id+2, value);  
+        if (document.getElementById(id + 1).value == value && document.getElementById(id + 2).value == value) {
+            victory = colorizeWinner(id, id + 1, id + 2, value);
         }//Horizontal
-        else if(document.getElementById(1).value == document.getElementById(4).value  && document.getElementById(4).value == document.getElementById(7).value){
-            victory = colorizeWinner(1, 4, 7,  value);  
+        else if (document.getElementById(1).value == document.getElementById(4).value && document.getElementById(4).value == document.getElementById(7).value) {
+            victory = colorizeWinner(1, 4, 7, value);
         }
-    }else if(id == 3 || id == 6 || id == 9){
-        if(id == 9){
-            if(document.getElementById(1).value == document.getElementById(5).value && document.getElementById(5).value  == document.getElementById(9).value){
-                victory = colorizeWinner(id, 1, 5, value);  
+    } else if (id == 3 || id == 6 || id == 9) {
+        if (id == 9) {
+            if (document.getElementById(1).value == document.getElementById(5).value && document.getElementById(5).value == document.getElementById(9).value) {
+                victory = colorizeWinner(id, 1, 5, value);
             }
-        } else if(id == 3){
-            if(document.getElementById(3).value == document.getElementById(5).value && document.getElementById(5).value  == document.getElementById(7).value){
-                victory = colorizeWinner(id, 5, 7, value);  
+        } else if (id == 3) {
+            if (document.getElementById(3).value == document.getElementById(5).value && document.getElementById(5).value == document.getElementById(7).value) {
+                victory = colorizeWinner(id, 5, 7, value);
             }
         }
-        if(document.getElementById(id - 1).value == value  && document.getElementById(id -2).value == value){
-            victory = colorizeWinner(id, id-1, id-2, value);  
-        }else if(document.getElementById(3).value == document.getElementById(6).value  && document.getElementById(6).value == document.getElementById(9).value){
-            victory = colorizeWinner(3, 6, 9, value);  
+        if (document.getElementById(id - 1).value == value && document.getElementById(id - 2).value == value) {
+            victory = colorizeWinner(id, id - 1, id - 2, value);
+        } else if (document.getElementById(3).value == document.getElementById(6).value && document.getElementById(6).value == document.getElementById(9).value) {
+            victory = colorizeWinner(3, 6, 9, value);
         }
-    }else if(id == 2 || id == 8){
-        if(document.getElementById(id - 1).value == value  && document.getElementById(id + 1).value == value){
-            victory = colorizeWinner(id, id-1, id+1, value);  
-        }else if(document.getElementById(2).value == document.getElementById(5).value  && document.getElementById(5).value == document.getElementById(8).value){
-            victory = colorizeWinner(2, 5, 8, value);  
+    } else if (id == 2 || id == 8) {
+        if (document.getElementById(id - 1).value == value && document.getElementById(id + 1).value == value) {
+            victory = colorizeWinner(id, id - 1, id + 1, value);
+        } else if (document.getElementById(2).value == document.getElementById(5).value && document.getElementById(5).value == document.getElementById(8).value) {
+            victory = colorizeWinner(2, 5, 8, value);
         }
     }
 }
 
 //Função para colorir os quadrantes alinhados e mostra alerta de vitoria
-function colorizeWinner(id1, id2, id3, value){
-    document.getElementById(id1).style.backgroundColor   = 'aqua'
-    document.getElementById(id2).style.backgroundColor   = 'aqua'
-    document.getElementById(id3).style.backgroundColor   = 'aqua'
-    
-    if(player1.value == value){
+function colorizeWinner(id1, id2, id3, value) {
+    document.getElementById(id1).style.backgroundColor = 'aqua'
+    document.getElementById(id2).style.backgroundColor = 'aqua'
+    document.getElementById(id3).style.backgroundColor = 'aqua'
+
+    if (player1.value == value) {
         console.log(value)
         playerPlays.innerHTML = `Vitória do jogador: ${player1.name}(${player1.value}) </br>
         Em ${Math.round((round + 1) / 2)} rodadas
         `
-    }else{
+    } else {
         playerPlays.innerHTML = `Vitória do jogador: ${player2.name}(${player2.value})</br>
         Em ${Math.round((round + 1) / 2)} rodadas
         `
@@ -104,16 +95,41 @@ function colorizeWinner(id1, id2, id3, value){
 }
 
 //Função para travar os inputs(Encerrar jogo)
-function crashInputs(){
-    let square =''
+function crashInputs() {
+    let square = ''
     let div = ''
-    for(var i = 1; i <= 9; i++){
+    for (var i = 1; i <= 9; i++) {
         square = `quadrado${i}`
         div = document.getElementById(i)
         document.getElementById(`${square}`).innerHTML = `
             <input id='${i}' onchange="pullValue(this.id)"
             value="${div.value}" style="background-color: ${div.style.backgroundColor};" readonly>
         `
-        console.log(div )
-        }
+        console.log(div)
+    }
+}
+
+//Função para setar valores ja inseridos
+function setValues(id) {
+    square = `quadrado${id}`
+    div = document.getElementById(id)
+    document.getElementById(`${square}`).innerHTML = `
+            <input id='${id}' onchange="pullValue(this.id)"
+            value="${div.value}" style="background-color: ${div.style.backgroundColor};" readonly>
+        `
+}
+
+//Função para mostrar paragrafo com valor no botão
+function showValue(id, button){
+    if (round % 2 == 0) {
+        button.value = player1.value
+        document.getElementById(`${id}`).innerHTML = `
+    <p> ${player1.value} </p>
+    `
+    } else {
+        button.value = player2.value
+        document.getElementById(`${id}`).innerHTML = `
+    <p> ${player2.value.substr(0, 1)} </p>
+    `
+    }
 }
--- jogoDaVelha/player.js
+++ jogoDaVelha/player.js
@@ -9,12 +9,18 @@ let player2 = {
 let round = 0
 let victory = false
 
+//Função para definir nome e caracter usado por cada jogador
 function definePlayers(){
     player1.name = window.prompt('Digite o nome do jogador 1')
-    player1.value = window.prompt(`Digite o caracter usado pelo jogador ${player1.name}`)
+    player1.value = window.prompt(`Digite o caracter usado pelo jogador ${player1.name}
+    (X ou O)
+    `).toUpperCase()
 
     player2.name = window.prompt('Digite o nome do jogador 2')
-    player2.value = window.prompt(`Digite o caracter usado pelo jogador ${player2.name}`)
+    player2.value = window.prompt(`Digite o caracter usado pelo jogador ${player2.name}
+    (X ou O)
+    `).toUpperCase()
+    errWhoPlays()
 }
 
 function contRounds(round){
@@ -23,6 +29,7 @@ function contRounds(round){
     return round
 }
 
+//Função para definir qual jogador esta ativo
 function whoPlays(round){
     if(round % 2 == 0){
         playerPlays.innerHTML = `Vez do jogador ${player1.name}(${player1.value})`
@@ -30,4 +37,25 @@ function whoPlays(round){
         playerPlays.innerHTML = `Vez do jogador ${player2.name}(${player2.value})`
     }
     
+}
+
+//Função de erros de definir jogadores 
+function errWhoPlays(){
+    if(player1.value == player2.value){
+        window.alert('Erro: Caracteres iguais. Ação sera repetida')
+        definePlayers()
+    }
+    if(player1.name == player2.name){
+        window.alert('Erro: Nomes iguais. Ação sera repetida')
+        definePlayers()
+    }
+    if(player1.value != 'X' && player1.value != 'O'){
+        window.alert(`Erro: Valor de caracter ${player1.value} do jogador ${player1.name} não aceito. Ação sera repetida`)
+        definePlayers()
+    }
+    if(player2.value != 'X' && player2.value != 'O'){
+        window.alert(`Erro: Valor de caracter ${player2.value} do jogador ${player2.name} não aceito. Ação sera repetida`)
+        definePlayers()
+    }
+
 }
